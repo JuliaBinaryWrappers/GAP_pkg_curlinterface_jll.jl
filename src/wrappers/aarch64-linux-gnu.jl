7b88c470839d253a93f82119b23edaefc5c379d8
jlbuild <juliabuildbot@gmail.com>
2025-06-17 00:06:59 +0000

    GAP_pkg_curlinterface_jll build 200.400.1+0

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -2,12 +2,11 @@
 export curl
 
 using GAP_jll
-using GAP_lib_jll
 using LibCURL_jll
 JLLWrappers.@generate_wrapper_header("GAP_pkg_curlinterface")
 JLLWrappers.@declare_file_product(curl)
 function __init__()
-    JLLWrappers.@generate_init_header(GAP_jll, GAP_lib_jll, LibCURL_jll)
+    JLLWrappers.@generate_init_header(GAP_jll, LibCURL_jll)
     JLLWrappers.@init_file_product(
         curl,
         "lib/gap/curl.so",
